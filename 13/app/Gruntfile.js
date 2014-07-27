2161fc7bcb525ef89938041276b57032c93b9b3c
driver <omari@crunchbang>
2014-07-27 14:27:19 -0400

    CSS updated

---
+++ 13/app/Gruntfile.js
@@ -0,0 +1,37 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '/* \n' +
+          ' * <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> \n' +
+          ' */ \n'
+    },
+    // CSSMin
+    cssmin: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      combine: {
+        files: '<%= pkg.css %>'
+      }
+    },
+    // Uglify
+    uglify: {
+      target: {
+        keepSpecialComments: 0,
+        banner: '<%= meta.banner %>',
+        files: '<%= pkg.js %>'
+      }
+    }
+  });
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task.
+  grunt.registerTask('default', [ 'uglify', 'cssmin' ]);
+
+};
